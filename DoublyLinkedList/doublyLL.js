16aeb152578ac3bb1e01dbd12fbae1cd716c2f08
Titan-Codes <vedansh1sharma2005@gmail.com>
2023-08-02 16:10:52 +0530

    shift fn in doubly LL

--- DoublyLinkedList/doublyLL.js
+++ DoublyLinkedList/doublyLL.js
@@ -55,6 +55,21 @@ class DoublyLinkedList {
             return this;
         }
     }
+
+    shift(){
+        if(this.length === 0){return undefined}
+        let temp = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+        this.length--;
+        return temp;
+    }
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(1)
