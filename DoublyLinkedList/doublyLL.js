6ce9c4de460da40c920f6e8192c7e293b31012f2
Titan-Codes <vedansh1sharma2005@gmail.com>
2023-08-02 16:01:30 +0530

    unshift fn in doubly LL

--- DoublyLinkedList/doublyLL.js
+++ DoublyLinkedList/doublyLL.js
@@ -41,6 +41,20 @@ class DoublyLinkedList {
         this.length--;
         return temp;
     }
+
+    unshift(value) {
+        const newNode = new Node(value)
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
+            this.length++;
+            return this;
+        }
+    }
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(1)
